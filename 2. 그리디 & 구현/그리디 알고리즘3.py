6c80382decdb3e97ff525fcb6556afbb6a291337
전은형 <ooooo5h91@gmail.com>
2022-02-18 22:53:26 +0900

    풀이법 재구현

--- 2. 그리디 & 구현/그리디 알고리즘3.py
+++ 2. 그리디 & 구현/그리디 알고리즘3.py
@@ -1,22 +1,27 @@
 # 모험가 길드
 
 def group(N, scared_score):
-     
-    # 공포도가 낮은 순서대로 정렬하기
-    sorted_scared_score = sorted(scared_score)
-    # print(sorted_scared_score)
     
-    result = 0 # 총 그룹의 수
-    count = 0  # 현재 그룹에 포함된 모험가의 수
+    # 낮은 공포도 순으로 정렬하면..
+    # 1,2,2,2,3 => 1은 패스, 2는2명이니까 2개 카운트, 그다음 냅두기
+    sorted_scared_score = sorted(scared_score)
+    
+    # 그룹수
+    count = 0
+    
+    # 횟수
+    result = 0
     
     for x in sorted_scared_score:
-        # 그룹에 모험가를 포함시키기
+        # 한명씩 꺼낼 때 마다 그룹수에 추가하기
         count += 1
+        
+        # 그룹멤버수가 공포도 x와 같거나 크다면 횟수 카운트
         if count >= x:
-            # 그룹에 포함시킨 모험가의 수가 공포도보다 크거나 같다면 그룹 결성
             result += 1
-            count = 0 # 그룹의 수를 측정했으면 다시 0명으로 리셋해줘야함
-    
-    return result        
+            # 횟수로 세고나면 다시 처음부터 그룹수를 세야하므로 초기화
+            count = 0
+            
+    return result
     
 print(group(5, [2,3,1,2,2]))
