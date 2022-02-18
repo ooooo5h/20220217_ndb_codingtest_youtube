b4f4e94e2c15d586ff24c791416bb86442c122c1
전은형 <ooooo5h91@gmail.com>
2022-02-18 23:57:01 +0900

    풀이법
    - 포인트1 : 이동계획을 리스트로 담아주는 방법
    - 포인트2 : 하나씩 꺼내서비교하는 방법(중첩for문)
    - 포인트3: continue

--- 2. 그리디 & 구현/구현2.py
+++ 2. 그리디 & 구현/구현2.py
@@ -1,8 +1,8 @@
 # 상하좌우
 # 이동 후 여행자의 위치값은?
-def current_location(N, plan):
+def current_location(N, plans):
     
-    plan = list(plan)
+    plans = list(plans)
         
     # 시작점
     x, y = 1, 1
@@ -10,26 +10,24 @@ def current_location(N, plan):
     # LRUD
     dx = [0,0,-1,1]
     dy = [-1,1,0,0]
+    move_types = ['L','R','U','D']
     
-    for letter in plan:
+    # 이동계획을 하나씩 꺼내서
+    for plan in plans:
         
+        for i in range(len(move_types)):
+            # 이동법은 4가지임
+            # 이동계획과 이동법의 i번째와 동일하다면
+            if plan == move_types[i]:
+                nx = x + dx[i]
+                ny = y + dy[i]
+                   
         # 크기를 벗어나는 움직임은 무시해야한다.
-        if not ((1 <= x <= N) or (1 <= y <= N)):
-            pass            
+        if not ((1 <= nx <= N) and (1 <= ny <= N)):
+            continue   # 반복문에서 continue는 continue아래의 코드는 무시하고 지나감
         
-        # 이동해야하는 설명서의 길이만큼 이동시키기           
-        if letter == 'L':
-            x = x + dx[0]
-            y = y + dy[0]
-        elif letter == 'R':
-            x = x + dx[1]
-            y = y + dy[1]
-        elif letter == 'U':
-            x = x + dx[2]
-            y = y + dy[2]
-        elif letter == 'D':
-            x = x + dx[3]
-            y = y + dy[3]
+        # 이동시키기
+        x,y = nx, ny
     
     return x,y      
 
