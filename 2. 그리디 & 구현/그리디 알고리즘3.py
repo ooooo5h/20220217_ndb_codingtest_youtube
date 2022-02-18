3ab63a2d857cba6586a080822b01b8469d48b670
전은형 <ooooo5h91@gmail.com>
2022-02-18 22:45:35 +0900

    풀이법
    - 포인트1 : 정렬해서 해결이 되는가?
    - 포인트2 : 변수의 갯수(총 그룹의 수는 생각했지만 그룹안에 있는 모험가의 수는 생각못했다)
    - 포인트3 : 몇명이 속해있는지 확인할 방법
    
    => 이제까지 배운 반복문과 조건문으로 충분히 풀 수 있었는데 아깝다..

--- 2. 그리디 & 구현/그리디 알고리즘3.py
+++ 2. 그리디 & 구현/그리디 알고리즘3.py
@@ -1,8 +1,22 @@
 # 모험가 길드
-N = int('모험가 수 :')
-scared_score = map(int, input('N명의 공포도의 값 : '))
 
 def group(N, scared_score):
      
-    # 공포도가 큰 순서로 묶기? 낮은 순서로 묶기?
-    # 묶고 난 다음에도 확인을 해야하는데.. 어떻게 해야 푸나
+    # 공포도가 낮은 순서대로 정렬하기
+    sorted_scared_score = sorted(scared_score)
+    # print(sorted_scared_score)
+    
+    result = 0 # 총 그룹의 수
+    count = 0  # 현재 그룹에 포함된 모험가의 수
+    
+    for x in sorted_scared_score:
+        # 그룹에 모험가를 포함시키기
+        count += 1
+        if count >= x:
+            # 그룹에 포함시킨 모험가의 수가 공포도보다 크거나 같다면 그룹 결성
+            result += 1
+            count = 0 # 그룹의 수를 측정했으면 다시 0명으로 리셋해줘야함
+    
+    return result        
+    
+print(group(5, [2,3,1,2,2]))
