d024045cfe1136c470df1448dc8c9c6babb1aa76
전은형 <ooooo5h91@gmail.com>
2022-02-18 22:27:06 +0900

    그리디 알고리즘 : 모험가 길드
    못풀었음..

---
+++ 2. 그리디 & 구현/그리디 알고리즘3.py
@@ -0,0 +1,8 @@
+# 모험가 길드
+N = int('모험가 수 :')
+scared_score = map(int, input('N명의 공포도의 값 : '))
+
+def group(N, scared_score):
+     
+    # 공포도가 큰 순서로 묶기? 낮은 순서로 묶기?
+    # 묶고 난 다음에도 확인을 해야하는데.. 어떻게 해야 푸나
