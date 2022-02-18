55821e0b0333ad56258641bafc7ca61997b3f6bb
전은형 <ooooo5h91@gmail.com>
2022-02-18 21:35:56 +0900

    그리디 알고리즘 : 최적의 해
    내가 푼 풀이

--- 2. 그리디 & 구현/그리디 알고리즘.py
+++ 2. 그리디 & 구현/그리디 알고리즘.py
@@ -1,1 +1,21 @@
 # 그리디 알고리즘 : 현재 상황에서 지금 당장 좋은것만 고르는 방법
+
+# 문제 : 1이 될 때 까지 수행해야하는 횟수의 최솟값은?
+
+def until_one(n, k):
+    
+    count = 0
+    
+    while n != 1 : 
+        
+        if n % k == 0:
+            n = n // k
+        else:
+            n = n - 1
+            
+        count += 1 
+    
+    return count
+    
+print(until_one(17,4))
+    
