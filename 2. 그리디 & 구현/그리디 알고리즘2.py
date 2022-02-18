72ba0846ac92387ae5358daf45c421f4ca5d84da
전은형 <ooooo5h91@gmail.com>
2022-02-18 22:00:07 +0900

    풀이법
    - result는 무조건 0일 필요 X.
    - 곱하기는 0,1은 비효율, 더하기는 더 큰 결과값을 만듦

--- 2. 그리디 & 구현/그리디 알고리즘2.py
+++ 2. 그리디 & 구현/그리디 알고리즘2.py
@@ -3,20 +3,19 @@
 
 def add_or_multiply(S):
     
-    a = list(map(int, S))
+    # 결과값을 무조건 0으로 대입하지않고, 문자열의 첫번째수를 변경하여 대입해두자    
+    result = int(S[0])
     
-    result = 0
-    
-    for i in range(len(S)):
-        if a[i-1] == 0:
-            result += a[i]
+    for i in range(1, len(S)):
+        
+        num = int(S[i])
+        
+        # 두번,세번째 숫자가 0이나 1. 혹은 result가 0혹은 1이라면 더하기를 하는 게 최적의 해!!
+        if num <= 1 or result <= 1:
+            result += num
         else:
-            result *= a[i]
+            result *= num
     
     return result
-    
 
 print(add_or_multiply('567'))
-# 틀린 코드
-# 02984는 제대로 정답이 나오지만
-# 567이면 0 * 5 = 0. 정답이 0으로 계속 리턴이 된다.
