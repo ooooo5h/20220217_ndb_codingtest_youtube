f06e4969857356639a7fb455e320b968be9981d1
전은형 <ooooo5h91@gmail.com>
2022-02-19 11:20:18 +0900

    풀이법
    - 이동해야하는 좌표를 한줄로도 표현 가능
    - 들어오는 c2를 숫자 좌표로 변경
    - 범위안에 있는 지 체크하는 방법은 한번 해봤으니 작성할 수 있었다

--- 2. 그리디 & 구현/구현4.py
+++ 2. 그리디 & 구현/구현4.py
@@ -3,18 +3,25 @@
 def count_cases(current_location):
     
     h = ['a','b','c','d','e','f','g','h']
-    알파벳좌표로 = h.index(current_location[0])+1
     
+    row = int(current_location[1])
+    column = h.index(current_location[0])+1
+    
+    # R,L,U,D
+    # 이동할 수 있는 8가지 방향을 한줄로 정리해도 된다
+    steps = [(-2,-1),(-2,1),(-1,-2),(-1,2),(1,2),(1,-2),(2,-1),(2,1)]
+        
     count = 0
     
-    # R,L,U, P
-    a = [0,0,-2,2]
-    b = [2,-2,0,0]
+    # 현재 위치를 이동할 수 있는 방향으로 이동시키기
+    for step in steps:
+        next_row = row + step[0]
+        next_column = column + step[1]
+        
+        # 범위 안에 들어온다면 카운트 해주면 됨
+        if 1 <= next_row <= 8 and 1 <=next_column <= 8:
+            count += 1
 
-    for i in range(1,9):
-        for j in range(1,9):
+    return count
             
-            # RLUP도 반복문을 계속 돌아야 할거 같은데.. ㅠㅠ
-            
-            
-count_cases('c2')
+print(count_cases('a1'))
