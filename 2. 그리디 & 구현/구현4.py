1699da6b8e9ab548cc2566cf3c08c7661414cd09
전은형 <ooooo5h91@gmail.com>
2022-02-19 11:30:12 +0900

    풀이법 재구현

--- 2. 그리디 & 구현/구현4.py
+++ 2. 그리디 & 구현/구현4.py
@@ -1,27 +1,24 @@
 # 왕실의 나이트
 
 def count_cases(current_location):
+
+    # a1, c2처럼 문자로 들어옴
+    y = int(current_location[1])
     
-    h = ['a','b','c','d','e','f','g','h']
+    alphabet = ['a','b','c','d','e','f','g','h']
+    x = alphabet.index(current_location[0])+1
     
-    row = int(current_location[1])
-    column = h.index(current_location[0])+1
+    steps = [(1,2),(1,-2),(2,-1),(2,1),(-2,-1),(-2,1),(-1,-2),(-1,2)]
     
-    # R,L,U,D
-    # 이동할 수 있는 8가지 방향을 한줄로 정리해도 된다
-    steps = [(-2,-1),(-2,1),(-1,-2),(-1,2),(1,2),(1,-2),(2,-1),(2,1)]
-        
     count = 0
     
-    # 현재 위치를 이동할 수 있는 방향으로 이동시키기
     for step in steps:
-        next_row = row + step[0]
-        next_column = column + step[1]
+        next_x = x + step[0]
+        next_y = y + step[1]
         
-        # 범위 안에 들어온다면 카운트 해주면 됨
-        if 1 <= next_row <= 8 and 1 <=next_column <= 8:
+        if 1 <= next_x <= 8 and 1 <= next_y <= 8 :
             count += 1
-
+    
     return count
             
-print(count_cases('a1'))
+print(count_cases('c2'))
