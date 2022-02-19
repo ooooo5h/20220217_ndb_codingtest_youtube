94461fd59213331272adceb61d4cc42f30dd0a96
전은형 <ooooo5h91@gmail.com>
2022-02-19 10:53:10 +0900

    풀이법
    - 가능한 모든 경우의 수를 세서 푸는 문제
    - 24시*60분*60초 = 86,400 하루는 86,400초라서 가능함
    - 완전탐색문제유형

--- 2. 그리디 & 구현/구현3.py
+++ 2. 그리디 & 구현/구현3.py
@@ -1,29 +1,21 @@
-
+# 시각
 
 def count_3(N):
     
-    start_hour = [0]
-    start_min = [min for min in range(0, 60)]
-    start_second = [sec for sec in range(0, 60)]
-
-    # 분 과 초는 동일하게 갈텐데
-    # 0~9 :  1개
-    # 10 ~ 19 : 1
-    # 20 ~ 29 : 1
-    # 30 ~ 39 : 10
-    # 40 ~ 49 : 1
-    # 50 ~ 59 : 1
-    # 1분간 15개
-    # 1초간 15개 => 1분 총 30개의 3이 등장 ..?
+    # 3의 갯수 카운트
+    count = 0
     
-    # 시
-    # 0시 30개
-    # 1시2시 60개
-    # 3시 30개
-    for i in range(N):
-        # 0,1,2,3,4
-        
-
+    for h in range(N+1):
+        # N+1인 이유 : 
+        # 5를 입력했으면, 0시 1시 .. 5시까지 돌아가며 3을 세야하니까
+        for m in range(60):
+            for s in range(60):
+                # 속해있는 모든 3의 개수를 세야하기 때문에
+                # 문자형이 편하다
+                # 반복문 중첩을 통해, 횟수를 돌때마다 3이 있다면 더해주면 해결
+                if '3' in str(h) + str(m) + str(s):
+                    count += 1
     
+    return count
         
-count_3(1)
+print(count_3(1))
