02962e4d901ca03383b04704ae96370587e1b705
전은형 <ooooo5h91@gmail.com>
2022-02-19 11:58:09 +0900

    풀이법
    - ''.join(문자열) => 문자를 붙여줌
    - 변수.isalpha() => 알파벳여부를 t/f로 리턴

--- 2. 그리디 & 구현/구현5.py
+++ 2. 그리디 & 구현/구현5.py
@@ -1,22 +1,29 @@
 # 문자열 재정렬
 from string import ascii_uppercase
 import string
+from unittest import result
 
 def make_beautiful(s):
-    
-    letters = list(s)
-    
-    num = 0
-    alphabet = ''
-    
-    for letter in letters:
-        if letter in str(list(range(0, 10))):
-            num += int(letter)
-        elif letter in list(string.ascii_uppercase):
-            alphabet += letter
-    
-    alphabet = ''.join(sorted(alphabet))
 
-    return alphabet+str(num)  
+    result = []
+    num = 0
+    
+    # 문자 하나씩 확인하기
+    for x in s:
+        # 알파벳이면 리스트에 넣기
+        if x.isalpha():
+            result.append(x)
+        
+        # 숫자면 더하기
+        else:
+            num += int(x)
+    
+    result.sort()
+    
+    # 숫자가 하나라도 있었다면 뒤에 붙이기
+    if num != 0:
+        result.append(str(num))
+    
+    return ''.join(result)   # ''.join(문자열)  => 문자열합치기
 
 print(make_beautiful('K1KA5CB7'))
