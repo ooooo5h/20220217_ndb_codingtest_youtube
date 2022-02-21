d2d2af79ed1e7384fd2f490527662a814a08de4e
전은형 <ooooo5h91@gmail.com>
2022-02-21 11:43:14 +0900

    풀이법
    - 100% 이해 못했음.. 코드가 안읽히는 데 좀 더 공부하고 다시 봐보기

--- 3. DFS/BFS/문제1_음료수얼려먹기.py
+++ 3. DFS/BFS/문제1_음료수얼려먹기.py
@@ -1,32 +1,40 @@
 # 음료수 얼려먹기
-n, m = map(int, input('N,M 입력: ').split(' '))
+# N, M을 공백 기준으로 구분해서 입력받기
+n,m = map(int, input().split())
 
-# 일단 아이스프레임도 이 값이 고정이 아니라 변경되어야함
-ice_frame = [
-    [0,0,1,1,0],
-    [0,0,0,1,1],
-    [1,1,1,1,1],
-    [0,0,0,0,0]
-]
+# 2차원 리스트의 맵 정보를 입력받기
+graph = []
+for i in range(n):  # N이 가로니까 가로 왼쪽에서 부터 값 입력하기
+    graph.append(list(map(int, input())))
+    
 
-# 모르겠는 부분
-# n,m을 어떻게 활용해야하는지
-# 위아래로 붙은 0들을 어떻게 연결..?ㅎㄷㄷ
-def count_icecream(n, m, ice_frame):
+# DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문하는 함수 생성
+def dfs(x,y) :
     
-    find_zero = 0
-    count = 0
+    # 주어진 범위를 벗어나는 경우 즉시 종료시키기
+    # 어떤 범위를 벗어난다는 거지...?
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
     
-    
-    for x in ice_frame:
-        for i in x:
-            if i == 0:
-                find_zero += 1
-            else:
-                if find_zero >= 1 :
-                    count += 1
-                find_zero = 0
-    
-    print(count)          
+    # 현재 노드 방문하지 않았다면 찾아가서 방문처리
+    if graph[x][y] == 0:
+        graph[x][y] = 1
         
-count_icecream(n,m,ice_frame)
+        dfs(x-1, y)  # 상
+        dfs(x+1, y)  # 하
+        dfs(x, y-1)  # 좌
+        dfs(x, y+1)  # 우의 위치들도 재귀적으로 호출하여 방문처리
+        return True
+    
+    return False  # 결과값을 트루, 펄스로...?왜?
+
+
+
+# 모든 노드에 대하여 음료수를 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j) == True:   # 트루면 왜 1을 체크하는거지..?
+            result += 1
+            
+print(result)
