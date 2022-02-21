da3e4e4aa65aee61f0c2d1bf96b03f7057536850
전은형 <ooooo5h91@gmail.com>
2022-02-21 15:45:52 +0900

    풀이법

--- 4.정렬알고리즘/문제1_두배열의원소교체.py
+++ 4.정렬알고리즘/문제1_두배열의원소교체.py
@@ -1,18 +1,19 @@
 n,k = map(int, input().split())
 
-a = []
-a.append(list(map(int, input().split())))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
-b = []
-b.append(list(map(int, input().split())))
+a.sort()   # a의 작은수를 b의 큰수와 바꾸면 되기 때문에 a는 작은수대로 정렬하고, b는 큰수대로 정렬
+b.sort(reverse=True)
 
-
-def change_num(a, b):
+# 첫번째 인덱스부터 확인해나가면서 두배열의 원소를 k번까지 비교해야함
+for i in range(k):
+    # a의 원소가 b원소보다 작으면 교환
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+        
+    # a의 원소가 b원소보다 크거나 같으면 반복문 탈출
+    else:
+        break
     
-    for i in range(0, n+1):
-        if a[i] > a[i+1]:
-            a[i], a[i+1] = a[i+1], a[i]
-    
-    print(a)
-                
-change_num(a,b)
+print(sum(a))
