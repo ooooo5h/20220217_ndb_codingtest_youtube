f9339f882049e18a2dad8719c5441972fdbebdbc
Jeon, Eun Hyung <78359232+ooooo5h@users.noreply.github.com>
2022-05-12 21:07:12 +0900

    재풀이 문제1 : 떡볶이떡만들기

--- 5.이진탐색/문제1_떡볶이떡만들기.py
+++ 5.이진탐색/문제1_떡볶이떡만들기.py
@@ -1,31 +1,23 @@
 # 떡볶이 떡 만들기
 # 떡의 개수 n, 떡의 길이 m
+n, m = map(int, input().split())
+rices = list(map(int, input().split()))
 
-def Count(len):
-    cnt = 0
+start = 0
+end = max(rices)
+result = 0
+while start <= end:
+    total = 0
+    mid = (start+end)//2
     for x in rices:
-        if x <= len:
-            cnt += 0
-        else:
-            cnt += x-len
-    return cnt
+        if x > mid:
+            total += x - mid
 
+    if total < m:
+        # 길이를 줄여야지
+        end = mid-1
+    else:   # 크거나 같다
+        result = mid
+        start = mid + 1
 
-n, m = map(int, input().split())   # 4 6
-rices = list(map(int, input().split()))   # 19 15 10 17
-
-# 적어도 m만큼의 떡을 집에 가져가기 위해
-# 설정할 수 있는 높이의 최댓값은?    # 정답은 15
-
-lt = 0
-rt = max(rices)
-res = 0
-while lt <= rt:
-    mid = (lt+rt)//2
-    if Count(mid) >= m:
-        res = mid
-        lt = mid + 1
-    else:
-        rt = mid - 1
-
-print(res)
+print(result)
